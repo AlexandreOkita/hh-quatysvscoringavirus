2578d1fc6f5773a580b83f9bd83052b141d040b8
Alexandre <xande.okita@gmail.com>
2020-05-23 19:13:40 -0300

    configure test server

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,8 @@
 const http = require('http');
 
-const hostname = '34.95.183.232';
-const port = 3300;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
+const port = process.env.PORT  || 3300;
+const server = http.createServer(() => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+server.listen(port)
