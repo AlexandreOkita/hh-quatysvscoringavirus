b569f5b5e64188da09f42ccc92c5f69da7f1c339
Alexandre <xande.okita@gmail.com>
2020-05-24 18:41:08 -0300

    add cors module

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const helloWorld = require('./helloWorld');
 const consumer = require('./api/routes/consumer');
 const deal = require('./api/routes/deal');
 const debugConsole = require('./api/middleware/consoleDebug');
 
+app.use('cors');
 app.use(debugConsole);
 app.use('/teste', helloWorld);
 app.use('/consumer', consumer);
