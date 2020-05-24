ccf0ce8fd773674617380347627f39fd0fa8f648
larissa.escaliante <larissa.escaliante@ekinops.com>
2020-05-24 15:45:06 -0300

    get deal method

--- backend/api/routes/deal.js
+++ backend/api/routes/deal.js
@@ -1,3 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const Deal = require('../../services/Deal');
+
+router.get('/getinfo', (req, res) => {
+
+  const id = req.body.iddeal;
+
+  Consumer.getDeal({id})
+      .then(response => res.status(200).send(response))
+      .catch(error => res.status(500).send(error));
+});
--- backend/services/Deal/index.js
+++ backend/services/Deal/index.js
@@ -5,6 +5,22 @@ const query = util.promisify(connection.query).bind(connection);
 
 class Deal{
 
+    static async getDeal({id}){
+        try{
+            let q = await query(
+                "SELECT * FROM consumer WHERE iddeal = ?",
+                [id]
+            );
+            console.log({message: 'Success', status: 200, deal: {id: q[0].idconsumer, name: q[0].name}});
+            return {message: 'Success', status: 200, deal: {interest_count: q[0].actualbuyers,
+                                                            interest_target: q[0].targetbuyers,
+                                                            target_price: q[0].targetprice,
+                                                            prazo: q[0].expiration}};
+        } catch (error){
+            console.log(error);
+            throw error;
+        }
+    }
 }
 
 module.exports = Deal;
