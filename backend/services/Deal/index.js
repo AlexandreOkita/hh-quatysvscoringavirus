3f04a4cc9432ed209671a9d333ad1d752d755e8e
Alexandre <xande.okita@gmail.com>
2020-05-24 20:08:20 -0300

    mais coisas pro getinfo

--- backend/services/Deal/index.js
+++ backend/services/Deal/index.js
@@ -25,11 +25,21 @@ class Deal{
                 "SELECT * FROM deal WHERE iddeal = ?",
                 [id]
             );
+            let q1 = await query(
+                `SELECT p.original_price as original_price, p.name as product_name, p.image as image, m.name as merchant_name
+                 FROM deal d JOIN product p ON d.product_idproduct = p.idproduct JOIN merchant m ON m.idmerchant = p.merchant_idmerchant
+                 WHERE iddeal = ?`, [id]
+            );
             console.log({message: 'Success', status: 200, deal: {idproduct: q[0].idproduct}});
             return {message: 'Success', status: 200, deal: {interest_count: q[0].actual_buyers,
                                                             interest_target: q[0].target_buyers,
                                                             target_price: q[0].target_price,
-                                                            prazo: q[0].expiration}};
+                                                            prazo: q[0].expiration,
+                                                            original_price: q1[0].original_price,
+                                                            product_name: q1[0].product_name,
+                                                            image: q1[0].image,
+                                                            merchant_name: q1[0].merchant_name
+                                                        }};
         } catch (error){
             console.log(error);
             throw error;
