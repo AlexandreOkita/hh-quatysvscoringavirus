baf87927ebac4903a7b26953ed8abcc5736f1ea1
Matheus S <matheus@pop-os.localdomain>
2020-05-24 12:36:34 -0300

    conserto de problemas após merge com a master

---
+++ hacka/src/screens/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from'../components/header/Header/Header'
+import TopCategories from '../components/topCategories/TopCategories'
+import Suggestions from '../components/suggestions/Suggestions'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <TopCategories />
+      <Suggestions />
+    </>
+  );
+}
+
+export default App;
