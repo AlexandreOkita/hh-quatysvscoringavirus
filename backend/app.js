22901c40f5ac8d93b8dd459e307fa093186467af
Alexandre <xande.okita@gmail.com>
2020-05-24 09:11:05 -0300

    add debug pipe

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const app = express();
 const helloWorld = require('./helloWorld');
 const consumer = require('./api/routes/consumer');
 
+console.log('DEBUG: Entrando na rota', req.url);
 app.use('/teste', helloWorld);
 app.use('/consumer', consumer);
 
