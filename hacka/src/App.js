448821809ca580c3fb218ee08a1e8d76c8da0e6c
CaioRhoden <caiorhoden@gmail.com>
2020-05-24 02:32:18 -0300

    Signup screen with not tested api + route in index.js

--- hacka/src/App.js
+++ hacka/src/App.js
@@ -1,6 +1,10 @@
-import React from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
