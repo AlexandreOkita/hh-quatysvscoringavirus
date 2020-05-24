b204dbc5e2bc055766ebc734c730924b643cbbf4
Alexandre Petrachini <alexandrepetrachini@gmail.com>
2020-05-24 11:41:44 -0300

    commit chatbot

--- hacka/src/index.js
+++ hacka/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Route, Link, BrowserRouter as Router } from 'react-router-dom'
+import ChatBot from 'react-simple-chatbot';
 import * as serviceWorker from './serviceWorker';
 
 //import your component (see SignUp as example)
@@ -17,6 +18,16 @@ const routing = (
     </div>
   </Router>
 )
+
+<ChatBot
+  steps={[
+    {
+      id: 'hello-world',
+      message: 'Hello World!',
+      end: true,
+    },
+  ]}
+/>
 
 ReactDOM.render(routing, document.getElementById('root'))
 // If you want your app to work offline and load faster, you can change
